2802423f384b3d0005d4633e2098e3c37bee40c7
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2024-08-14 15:41:00 +0200

    feat: Add environment variable util (#17)
    * feat: Add environment variable util
    
    Adds a utility function that takes an array of
    process.env. keys, which throws if any of the values is missing.
    Optionally, for some keys defaults can be provided.
    
    * fix: Export new function

---
+++ src/environmentVariables.ts
@@ -0,0 +1,40 @@
+/**
+ * Check if all required environment variables are set
+ *
+ * Optionally, some values can be provided a default value. Usage:
+ * If you need 'process.env.SOME_CONFIG_PARAM` to be set:
+ *
+ * ```const env = environmentVariables['SOME_CONFIG_PARAM']```
+ *
+ * Then use `env.SOME_CONFIG_PARAM`. Will throw if process.env.SOME_CONFIG_PARAM
+ * is not set. If you want to set a default value, use as follows:
+ *
+ * ```const env = environmentVariables['SOME_CONFIG_PARAM, SOME_OPTIONAL_PARAM', { 'SOME_OPTIONAL_VALUE': 'someDefault' }]```
+ *
+ * @param keys the environment keys that need to be set
+ * @param defaults default values can be provided for optional keys
+ * @returns
+ */
+export function environmentVariables<T extends readonly string[]>(
+  keys: T,
+  defaults: Partial<Record<T[number], string>> = {}): {
+    [K in T[number]]: string;
+  } {
+  const env = {} as {
+    [K in T[number]]: string;
+  };
+
+  keys.forEach((key) => {
+    // Type assertion to ensure TypeScript understands key is a valid key for defaults
+    const typedKey = key as T[number];
+    const value = process.env[typedKey] ?? defaults[typedKey] ?? '';
+
+    if (!value) {
+      throw new Error(`Environment variable ${typedKey} is missing`);
+    }
+
+    env[typedKey] = value;
+  });
+
+  return env;
+}
